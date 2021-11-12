971342d43b56e13fa7a4ccec193710c6205f8a35
subrataSamartha <subrata.samartha2019@gift.edu.in>
2021-11-12 16:03:28 +0530

    hello.java modified

--- src/hello.java
+++ src/hello.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class hello {
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello worlds");
+        System.out.println("This is file is modified");
+    }
 }
